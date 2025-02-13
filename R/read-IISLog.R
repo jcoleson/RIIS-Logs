3fd9d171b889252ccc41e09e6d4bab6837aa214c
Jared <jcoleson@users.noreply.github.com>
2025-02-13 17:03:23 -0600

    Update read-IISLog.R

--- R/read-IISLog.R
+++ R/read-IISLog.R
@@ -16,22 +16,33 @@ read.IISLog <- function(filePath, tz="America/Chicago") {
   # read the actual log file
   iisLogFile = read.csv(filePath, header = FALSE,sep=' ',comment.char="#",blank.lines.skip=TRUE, quote = "")
   colnames(iisLogFile) = gsub("-|\\(|\\)","_",unlist(columnNames[1,])) #replace - with _ in column names
+
+  # Ensure necessary columns exist before processing
+  if (all(c("date", "time") %in% colnames(iisLogFile))) {
+    # create local_datetime column that combines date and time and switch UTC to local (central) time
+    iisLogFile$local_datetime <- safe_as_POSIXct(paste(iisLogFile$date, iisLogFile$time, sep=" "))
+    iisLogFile$date=safe_as_Date(iisLogFile$date)
+    attributes(iisLogFile$local_datetime)$tzone <- tz
+    
+    if ("time_taken" %in% colnames(iisLogFile)) {
+      iisLogFile$time_taken_s <-  iisLogFile$"time_taken" / 1000
+      iisLogFile$datetimeStarted <- iisLogFile$local_datetime - iisLogFile$time_taken_s
+      iisLogFile$datetimeEnded <- iisLogFile$local_datetime
+    }
   
-  # create local_datetime column that combines date and time and swithc UTC to local (central) time
-  iisLogFile$local_datetime <- as.POSIXct(paste(iisLogFile$date,iisLogFile$time, sep=" "), tz="UTC") 
-  iisLogFile$date=as.Date(iisLogFile$date)
-  attributes(iisLogFile$local_datetime)$tzone =  tz
-  
-  if ("time_taken" %in% colnames(iisLogFile)) {
-    iisLogFile$time_taken_s <-  iisLogFile$"time_taken" / 1000
-    iisLogFile$datetimeStarted <- iisLogFile$local_datetime - iisLogFile$time_taken_s
-    iisLogFile$datetimeEnded <- iisLogFile$local_datetime
   }
-  
+
   if ("sc_bytes" %in% colnames(iisLogFile)) {
-    iisLogFile$Kbytes <- round((iisLogFile$sc_bytes / 1024), digits = 2 )  
-    iisLogFile$Mbytes <- round(((iisLogFile$sc_bytes / 1024) / 1024), digits = 2 )
-  }
+      iisLogFile$Kbytes <- round((iisLogFile$sc_bytes / 1024), digits = 2 )  
+      iisLogFile$Mbytes <- round(((iisLogFile$sc_bytes / 1024) / 1024), digits = 2 )
+    }
   
   return(iisLogFile)
+}
+
+safe_as_POSIXct <- function(x, pTz = "UTC") {
+  tryCatch(as.POSIXct(x, tz = pTz), error = function(e) NA)
+}
+safe_as_Date <- function(x) {
+  tryCatch(as.Date(x), error = function(e) NA)
 }
